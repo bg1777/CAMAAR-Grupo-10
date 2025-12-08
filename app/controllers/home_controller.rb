ae802d5339367a2a2f267018767361a4e8f2a297
bg1777 <bgrodrigues2005@gmail.com>
2025-12-07 21:55:14 -0300

    Login de admin com sucesso

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,13 @@
 # app/controllers/home_controller.rb
 
 class HomeController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
-    if user_signed_in?
-      if current_user.admin?
-        redirect_to admin_root_path
-      else
-        @user = current_user
-      end
+    if current_user.admin?
+      redirect_to admin_root_path
+    else
+      @user = current_user
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,14 @@
 # config/routes.rb
 
 Rails.application.routes.draw do
-  get "home/index"
-  devise_for :users
+  devise_for :users, skip: [:registrations]  # Desabilita registro nas rotas
   
-  # Root path
   root "home#index"
   
-  # Admin dashboard
   namespace :admin do
-    get "users/index"
-    get "users/show"
-    get "users/edit"
-    get "users/update"
-    get "users/destroy"
-    get "dashboard/index"
     root "dashboard#index"
     resources :users
   end
   
-  # Home public
   get "home", to: "home#index"
 end
