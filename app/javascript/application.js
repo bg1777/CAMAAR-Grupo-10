fa3bf4c8ce715b10d04723b1b605eb2776e25dbf
Maria Carolina <carolina.burgum@gmail.com>
2025-12-09 00:24:48 -0300

    feat/adicionar campo ao editar template ja existente

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "bootstrap"
+// import "bootstrap"
+
+window.add_fields = function(link, association, content) {
+  var new_id = new Date().getTime();
+  var regexp = new RegExp("new_" + association, "g");
+  
+  // Insere o novo campo ANTES do botão clicado
+  link.insertAdjacentHTML('beforebegin', content.replace(regexp, new_id));
+}
